8b1fd74376116c023540eabeedb267523ab4e5de
Lucas-Angelo <lcs2001_@hotmail.com>
2020-07-10 15:46:30 -0300

    tipo de variável Map

---
+++ Hello/varTipoMaps.dart
@@ -0,0 +1,9 @@
+void main() {
+  Map usuario = {
+    'nome': 'Lucas',
+    'idade': 18,
+    'altura': 1.81,
+    'Lucas-Angelo': true,
+  };
+  print(usuario); //usuario.length para mostrar o tanto de dados
+}
