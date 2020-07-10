52ac90755479190accf20cff429b6e9f9cdf5e6c
Lucas-Angelo <lcs2001_@hotmail.com>
2020-07-10 20:19:40 -0300

    chamando dados de lists e maps

--- Hello/varTipoList.dart
+++ Hello/varTipoList.dart
@@ -1,4 +1,6 @@
 void main() {
   List listaProdutos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'arroz', 1.5, true];
-  print(listaProdutos); //listaProdutos.length para mostrar o tanto de dados
+  print(listaProdutos);
+  print(listaProdutos.length); //Mostra a quantidade de dados
+  print(listaProdutos[10]); //Mostrar informação da posição 10
 }
--- Hello/varTipoMaps.dart
+++ Hello/varTipoMaps.dart
@@ -4,6 +4,10 @@ void main() {
     'idade': 18,
     'altura': 1.81,
     'Lucas-Angelo': true,
+    1: 'Tipo inteiro',
   };
-  print(usuario); //usuario.length para mostrar o tanto de dados
+  print(usuario);
+  print(usuario.length); //Mostra a quantidade de dados
+  print(usuario['nome']); //Buscar um dado específico
+  print(usuario[1]); //Forma de chamar um dado específico não string
 }
