a01518557079cc188e66ec50ba7fff2c5ba148df
Lucas-Angelo <lcs2001_@hotmail.com>
2020-07-11 11:36:40 -0300

    Fim, funções com return e passagem de parâmetro

--- Hello/funcoes.dart
+++ Hello/funcoes.dart
@@ -1,8 +1,19 @@
 void main() {
   boasVindas();
+  double resSimulador = simulador(56.55, 47.80);
+  print(resSimulador);
+
+  print(calcArea(2));
 }
 
 void boasVindas() {
   String nome = 'Lucas';
   print('Welcome $nome!');
 }
+
+double simulador(double valor1, double valor2) {
+  double res = valor1 + valor2;
+  return res;
+}
+
+double calcArea(double raio) => 3.14 * raio * raio;
