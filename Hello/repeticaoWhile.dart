879e95a9f77e7a2b4262db0231a0590e338c83b3
Lucas-Angelo <lcs2001_@hotmail.com>
2020-07-11 11:28:40 -0300

    estrutura de repetição do-while

--- Hello/repeticaoWhile.dart
+++ Hello/repeticaoWhile.dart
@@ -1,7 +1,14 @@
 void main() {
   int idade = 10;
+
   while (idade < 18) {
     print(idade.toString() + ' é menor que dezoite anos');
     idade++;
   }
+
+  //Do while executa pelo menos uma vez
+  do {
+    print('Teste Do While');
+    idade += 20;
+  } while (idade < 60); //Se colocar false aqui, aparece o teste uma vez
 }
