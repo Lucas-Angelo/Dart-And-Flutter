729e483e5988a7886091ec3e11e9194e51b18ca7
lcs2001 <lcs2001_@hotmail.com>
2021-01-17 11:57:44 -0300

    inicio da main

---
+++ Flutter/projetos/tinder_login_page/conversor/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+main() {
+  return (AppWidget);
+}
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+    );
+  }
+}
