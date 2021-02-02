b166c1c8cc2c6d0776d8f7fd959f987a7ce47f1b
lcs2001 <lcs2001_@hotmail.com>
2021-02-02 09:35:48 -0300

    funcoes async utilizando await e package http

--- Dart/Assincrona/dartassincrona/bin/dartassincrona.dart
+++ Dart/Assincrona/dartassincrona/bin/dartassincrona.dart
@@ -1,7 +1,18 @@
 import 'package:dartassincrona/dartassincrona.dart' as dartassincrona;
 
 void main(List<String> arguments) {
+  print4();
+  print('1');
+  print('2');
+  print('3');
+}
 
+Future<void> print4() async {
+  await Future.delayed(Duration(seconds: 2));
+  print('4');
+}
+
+void usandoFuture() {
   var future = Future.delayed(Duration(seconds: 2));
   future.then((value) => print('4'));
 
